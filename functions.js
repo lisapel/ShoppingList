bc267d29c18599be0ecf029301800e08d075a27b
lisapel <lisa.peldan@gmail.com>
2023-03-17 11:13:19 +0100

    first commit

---
+++ functions.js
@@ -0,0 +1,31 @@
+function addItem() {
+    var newItem = document.getElementById("myInput").value;
+    var listItem = document.createElement("li");
+    var text = document.createTextNode(newItem);
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    listItem.appendChild(text);
+    listItem.appendChild(checkbox);
+    if (newItem === '') {
+        alert("You cannot put a empty thing to grocery list.")
+    } else {
+        listItem.classList.add("list-group-item", "list-group-item-action");
+        document.getElementById("list").appendChild(listItem);
+    }
+    document.getElementById("myInput").value = "";
+    checkbox.addEventListener("click", shopped);
+}
+
+var checkboxElems = document.querySelectorAll("input[type='checkbox']");
+
+for (var i = 0; i < checkboxElems.length; i++) {
+    checkboxElems[i].addEventListener("click", shopped);
+}
+function shopped() {
+    var listItem = this.parentNode;
+    if (this.checked) {
+        listItem.style.textDecoration = "line-through";
+    } else {
+        listItem.style.textDecoration = "none";
+    }
+}
